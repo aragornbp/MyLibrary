bebb4acd2bf9dbffd8f5f2398aec0f234fcbe3fd
Bruno Paulin <bruno.p-@hotmail.com>
2023-07-30 21:10:04 -0300

    base

---
+++ app/src/main/java/com/example/mylibrary/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.mylibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnAllBooks, btnAlreadyRead, btnWantToRead, btnCurrentyReading, btnFavorite, btnAbout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        btnAllBooks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AllBooksActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    private void initViews() {
+        btnAllBooks = findViewById(R.id.btnAllBooks);
+        btnAlreadyRead = findViewById(R.id.btnAlreadyReady);
+        btnWantToRead = findViewById(R.id.btnWantToRead);
+        btnCurrentyReading = findViewById(R.id.btnCurrentReading);
+        btnFavorite = findViewById(R.id.btnFavorite);
+        btnAbout = findViewById(R.id.btnAbout);
+    }
+}
